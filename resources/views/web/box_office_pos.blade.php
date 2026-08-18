fec67598b8701b216001c8e58340bfd8a552baae
Deivid Wilmer <dave@capacitaperu.com>
2026-08-18 14:39:10 -0400

    comprador y dni restaurado_v2

--- resources/views/web/box_office_pos.blade.php
+++ resources/views/web/box_office_pos.blade.php
@@ -1302,6 +1302,12 @@ const lum = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
                         return html.replace(inlineRegex, `$1${newValue}`);
                     }
 
+                    // Patrón 4: <tag ...>LABEL: VALOR</tag> (ej. <div ...>COMPRADOR: CHRISTIAN GOMEZ</div> o DNI: 70436491)
+                    const singleTagRegex = new RegExp(`(${labelPattern}:?\\s*)([^<]+)`, 'i');
+                    if (singleTagRegex.test(html)) {
+                        return html.replace(singleTagRegex, `$1${newValue}`);
+                    }
+
                     return html;
                 }
 
